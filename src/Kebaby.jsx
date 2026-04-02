eefb793b13de5ab34f715dbe0df18ff12429451b
MikolajPotiopa <mikolaj.potiopa.biznes@gmail.com>
2026-04-02 18:52:20 +0200

    Rozne kebaby

--- src/Kebaby.jsx
+++ src/Kebaby.jsx
@@ -1,6 +1,19 @@
-export default function Kebab()
+export default function Kebab({kebab})
 {
-    return(
-        <h1>Kebab : D</h1>
-    );
+    const theme ={
+        backgroundColor: 'black',
+        color: 'red'
+    };
+
+
+    if (kebab.sos=='Spicy')
+    {
+        return <h1 style={theme} > Kebab:  {kebab.size} {kebab.meat} {kebab.sos}🔥 </h1>
+    }else if(kebab.sos=='Mild')
+    {
+        return <h1 style={theme} > Kebab:  {kebab.size} {kebab.meat} {kebab.sos}🍼 </h1>
+    }else if(kebab.sos == "Mix")
+    return <h1 style={theme} > Kebab:  {kebab.size} {kebab.meat} {kebab.sos}🖖 </h1>
+        
+    
 }
