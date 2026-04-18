7e23d96a93b250763fc10937c45c1c675bff7603
MikolajPotiopa <mikolaj.potiopa.biznes@gmail.com>
2026-04-18 11:59:42 +0200

    18.04.2026 poprawka 2

--- src/tablesOfData/Sauces.jsx
+++ src/tablesOfData/Sauces.jsx
@@ -2,3 +2,27 @@ export const textForSauces ={
     Title:"Wybierz sosy",
     SubTitle:"Wybierz max 2"
 }
+
+export const sauces=[
+    {
+        sauce:"Sos czosnkowy"
+    },
+    {
+        sauce:"Sos ostry(chili)"
+    },
+    {
+        sauce:"Sos mieszany"
+    },
+    {
+        sauce:"Sos jogurtowy"
+    },
+    {
+        sauce:"Sos barbecue"
+    },
+    {
+        sauce:"Sos curry"
+    },
+    {
+        sauce:"Sos tahini"
+    },
+]
