03700d146019959556a8be1c76d3769e1ea09619
MikolajPotiopa <mikolaj.potiopa.biznes@gmail.com>
2026-04-21 15:46:37 +0200

    21.04.2026 poprawka 1

--- src/Main/Cart.jsx
+++ src/Main/Cart.jsx
@@ -1,11 +1,22 @@
+
+import { useEffect, useState,motion } from "react";
 import { FaShoppingCart } from "react-icons/fa";
 
 export default function Cart(){
+    const [isOpen,setOpen] = useState(false);
+    const onClose =()=>{
+        setOpen(!isOpen);
+    }
+
     return(
         <div className="stickyCart">
+            {isOpen?(
             <button className="cartButton">
                 <FaShoppingCart className="cartIcon"/>
             </button>
+            ):<div/>
+            }
+            
             
         </div>
     );
